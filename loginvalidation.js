e8bec1df2e39eef8f79752306d742d73b8f59a6f
Akshay <akshay_92@live.com>
2017-03-20 23:37:25 -0400

    Please verify this

--- loginvalidation.js
+++ loginvalidation.js
@@ -1,15 +1,22 @@
 
 
-function validateForm() {
-        var un = document.loginform.fname.value;
-        var pw = document.loginform.lname.value;
-        var username = "username"; 
-        var password = "password";
-        if ((un == username) && (pw == password)) {
-           
-            return false;
-        }
-        else {
-            alert ("Login was unsuccessful, please check your username and password");
-        }
-      }
+function validate()
+{
+ var usrname = document.forms["login"]["user"].value;
+ var passworde = document.forms["login"]["pass"].value;
+
+ if(usrname.length < 5)
+ {
+  window.alert("Username Invalid!!Should be atleast 5 character in length");
+  return false;
+ }
+ else if(passworde.length < 4)
+ {
+  window.alert("Password must be 4 charecter in length");
+  return false;
+ }
+ else
+ {
+  return true;
+ }
+}  
